3ea74cd9be08126690dd9a06a6805ecdfff99d6d
SetyoRian <setyo.wahyu.trianto@gmail.com>
2022-09-15 21:23:13 +0700

    complete project locally

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 // init project
 var express = require('express');
+var moment = require('moment');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
@@ -24,9 +25,34 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date", (req, res) => {
+  const params = req.params.date;
+  if (isNaN(params)) {
+    const checkDate = moment(params).isValid();
+    if (checkDate === false) {
+      res.status(400).json({ error: "Invalid Date" });
+      throw new Error("Invalid Date");
+    } else {
+      let unixDate = Date.parse(params);
+      let utcDate = new Date(params).toUTCString();
+      res.json({ unix: unixDate, utc: utcDate });
+    }
+  } else {
+    let unixToUtc = new Date(params * 1000).toUTCString();
+    let unixNumber = Number(params);
+    res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
+  }
+})
+
+app.get('/api', (req, res) => {
+  const unixDate = Date.now();
+  let utcDate = new Date(unixDate).toUTCString();
+  res.json({unix: unixDate, utc: utcDate});
+})
+
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
